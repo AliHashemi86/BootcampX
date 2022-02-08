cb83da3bab95b5c312c9c8d6f0e518560fe047a3
Ali Hashemi <alihashemmi.ca@gmail.com>
2022-02-07 19:04:01 -0500

    add queries3

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS assignment_submissions;
 DROP TABLE IF EXISTS assignments;
 
-
 CREATE TABLE assignments (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -5,6 +5,7 @@ start_date DATE,
 end_date DATE
 );
 
+
 CREATE TABLE students (
 id SERIAL PRIMARY KEY NOT NULL,
 name VARCHAR(255) NOT NULL,
@@ -15,4 +16,3 @@ start_date DATE,
 end_date DATE,
 cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
-
